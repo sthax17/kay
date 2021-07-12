9737284d07f1208f74f33c2d0fc032edff425335
sthax17 <sthax17@gmail.com>
2021-07-12 11:18:47 +0900

    7/12 2 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -26,7 +26,7 @@ def hello_world(request):
                      context={'hello_world_list': hello_world_list})
 
 class AccountCreateView(CreateView):
-    model - User
+    model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
